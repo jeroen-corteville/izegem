f51b0ad6387d660ef96a254a685c0111a608cd48
jeroen-corteville <jeroencorteville@gmail.com>
2014-12-26 20:22:00 +0100

    [first commit]

---
+++ app/js/app.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+/* App Module */
+
+var izegemApp = angular.module('izegemApp', [
+    'ngRoute',
+
+    'izegemControllers'
+]);
+
+izegemApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+        when('/home', {
+            templateUrl: 'partials/homepage.html',
+            controller: 'HomepageCtrl'
+        }).
+        when('/gallery', {
+            templateUrl: 'partials/gallery.html',
+            controller: 'GalleryCtrl'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+    }
+]);
---
+++ app/js/controllers.js
@@ -0,0 +1,36 @@
+﻿'use strict';
+
+/* Controllers */
+
+var izegemControllers = angular.module('izegemControllers', []);
+
+izegemControllers.controller('IzegemAppCtrl', ['$scope', 'Phone',
+  //function ($scope, Phone) {
+  //    $scope.phones = Phone.query();
+  //    $scope.orderProp = 'age';
+  //}
+]);
+
+izegemControllers.controller('HomepageCtrl', ['$scope',
+    function ($scope) {
+    
+
+    }]);
+
+izegemControllers.controller('GalleryCtrl', ['$scope',
+    function ($scope) {
+
+
+    }]);
+
+izegemControllers.controller('HomepageCtrlazerty', ['$scope', '$routeParams', 'Phone',
+  //function ($scope, $routeParams, Phone) {
+  //    $scope.phone = Phone.get({ phoneId: $routeParams.phoneId }, function (phone) {
+  //        $scope.mainImageUrl = phone.images[0];
+  //    });
+
+  //    $scope.setImage = function (imageUrl) {
+  //        $scope.mainImageUrl = imageUrl;
+  //    }
+  //
+]);
