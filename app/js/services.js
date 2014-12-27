b7145aed2ead8b0fe794722c1ff785eca6355c4c
jeroen-corteville <jeroencorteville@gmail.com>
2014-12-27 18:30:50 +0100

    commit

---
+++ app/js/services.js
@@ -0,0 +1,8 @@
+﻿var izegemServices = angular.module('izegemServices', ['ngResource']);
+
+izegemServices.factory('Locatie', ['$resource',
+    function ($resource) {
+        return $resource('locaties/:locatieId.json', {}, {
+            query: { method: 'GET', params: { locatieId: 'locaties' }, isArray: true }
+        });
+}]);
