f9629e628584787e995cc12b6e8a2fc9e8748c6b
jeroen-corteville <jeroencorteville@gmail.com>
2015-01-02 12:53:31 +0100

    QuizApp verdere uitwerking

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -54,14 +54,83 @@ izegemControllers.controller('QuizCtrl', ['$scope','$http',
         $('#quiz').addClass('active');
         $('#about').removeClass('active');
 
+        $scope.score = 0;
+        $scope.aantalVragen = 0
+
         $scope.vragen = [];
 
+        var selecAntwoorden = [];
+        var AlleAntwoorden = []
+
         var onVragenDownloaded = function (response) {
-            angular.forEach(response.data.Vragen.vragen, function (value, key) {
-                var newVraag = new Vraag(value.Vraag, value.Foto, value.CorrectAntwoord, value.FouteAntwoorden);
-                $scope.vragen.push(newVraag);
+            //Hier moet de quiz geinitializeerd worden. 10 random vragen worden getrokken tussen alle vragen die aanwezig zijn.
+
+            AlleAntwoorden = response.data.Vragen.vragen;
+            var AlleAntwoordenLength = response.data.Vragen.vragen.length;
+            
+            //10 random vraagnummers in Array steken:
+            while (selecAntwoorden.length < 10) {
+                var randomNumber = Math.ceil(Math.random() * AlleAntwoordenLength)
+                console.log(randomNumber);
+                var found = false;
+                for (var i = 0; i < selecAntwoorden.length; i++) {
+                    console.log('Geselecteerd Antwoordnummer: ' + selecAntwoorden[i]);
+                    console.log('RandomNummer: ' + randomNumber);
+                    if (selecAntwoorden[i] == randomNumber) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) selecAntwoorden[selecAntwoorden.length] = randomNumber;
+            }
+            console.log(selecAntwoorden);
+            volgendeVraag();
+        }
+
+        var AddClickEventsAntwoorden = function () {
+            $('.antwoord').click(function () {
+                if ($(this).hasClass('correct')) {
+                    $(this).css({
+                        'background-color': 'green',
+                        'color': 'white'
+                    });
+                    $('.antwoord').unbind('click');
+                    $scope.score = $scope.score + 1
+                }
+                else {
+                    $(this).css({
+                        'background-color': 'red',
+                        'color': 'white'
+                    });
+                    $('.antwoord').unbind('click');
+                    ToonJuistAntwoord();
+                }
+                $scope.aantalVragen = $scope.aantalVragen + 1;
+                $scope.$apply();
+                setTimeout(volgendeVraag, 500);
             });
-            setTimeout(RandomizeAntwoorden,0)
+            
+        }
+
+        var volgendeVraag = function () {
+            console.log('laden van de volgende vraag');
+            console.log(selecAntwoorden[0]);
+            if (selecAntwoorden[0]) {
+                var Opgehaaldevraag = AlleAntwoorden[selecAntwoorden[0] - 1];
+                console.log(AlleAntwoorden[selecAntwoorden[0] - 1]);
+                var newVraag = new Vraag(Opgehaaldevraag.Vraag, Opgehaaldevraag.Foto, Opgehaaldevraag.CorrectAntwoord, Opgehaaldevraag.FouteAntwoorden);
+                $scope.vragen.splice($scope.vragen.indexOf(newVraag),1);
+                $scope.vragen.push(newVraag);
+                selecAntwoorden.splice(0, 1);
+                
+                setTimeout(AddClickEventsAntwoorden, 5);
+                setTimeout(RandomizeAntwoorden,10);
+            }
+        }
+
+        var ToonJuistAntwoord = function () {
+            console.log('nu moet het juiste antwoord verschijnen');
+            $('.correct').css({ 'background-color': 'green' });
         }
         
         //var antwoorden = document.querySelector('.antwoorden');
@@ -74,7 +143,6 @@ izegemControllers.controller('QuizCtrl', ['$scope','$http',
         }
 
         $http.get('quizvragen/quizvragen.json').then(onVragenDownloaded, onError);
-        
 
         var RandomizeAntwoorden = function () {
             var Antwoorden = document.querySelector('.antwoorden');
