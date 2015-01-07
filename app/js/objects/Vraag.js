be05cdcbf166931b81ffb8b180b2b0a8570a4ed2
jeroen-corteville <jeroencorteville@gmail.com>
2015-01-07 14:00:40 +0100

    vraag terug na per ongeluk verwijderd te hebben

---
+++ app/js/objects/Vraag.js
@@ -0,0 +1,14 @@
+﻿
+function Vraag(vraag,foto,correctantwoord,fouteantwoorden){
+    this.vraag = vraag;
+    this.foto = foto;
+    this.correctantwoord = correctantwoord;
+    this.fouteantwoorden = fouteantwoorden;
+}
+
+Vraag.prototype = {
+    get Vraag() { return this.vraag },
+    get Foto() { return this.foto },
+    get CorrectAntwoord() { return this.correctantwoord },
+    get FouteAntwoorden(){return this.fouteantwoorden}
+}
